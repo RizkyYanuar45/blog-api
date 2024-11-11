8f314104552c04984ccadb9220db57f8f18e40c1
RizkyYanuar45 <rizkyanuar4@gmail.com>
2024-11-12 00:07:31 +0700

    push

---
+++ src/servers.js
@@ -0,0 +1,10 @@
+import express from "express";
+import mongoose from "mongoose";
+const app = express();
+const port = 3000;
+
+mongoose.connect("mongodb://localhost:27017/blogDB");
+
+app.listen(port, (req, res) => {
+  console.log(`Server is running on port http://localhost:${port}`);
+});
