fda76ee857e1cf1207efb5abc8c684d1ad1b50bc
RizkyYanuar45 <rizkyanuar4@gmail.com>
2024-11-13 09:44:06 +0700

    create blog post end point

--- src/servers.js
+++ src/servers.js
@@ -1,10 +1,20 @@
 import express from "express";
 import mongoose from "mongoose";
+import createPost from "./routes/post.js";
+
 const app = express();
 const port = 3000;
 
-mongoose.connect("mongodb://localhost:27017/blogDB");
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(port, (req, res) => {
-  console.log(`Server is running on port http://localhost:${port}`);
+app.use("/api/v1/blog", createPost);
+
+app.get("*", (req, res) => {
+  res.status(404).send("page not found");
 });
+
+mongoose
+  .connect("mongodb://localhost:27017/blogDB")
+  .then(() => app.listen(port))
+  .then(() => console.log(`Server is running on port http://localhost:${port}`))
+  .catch((err) => console.log(`connection error: ${err}`));
